468bdb4335e1cf8f7d0f77eaf276b15079080084
Adam T <ataras2@gmail.com>
2022-06-23 19:08:55 +1000

    added correct handling of subset and thresholding around surface in pipeline

---
+++ ToFFpipeline/checkSurface.m
@@ -0,0 +1,25 @@
+function [error, P, subset] = checkSurface(Pz, K, ToFFarr, LFsize, dLF_fn, pixel)
+    % resample the LF to shift it correctly 
+    grid = getInterpGrid(K, ToFFarr, LFsize, Pz, pixel);
+    depthSamples = dLF_fn(grid{2},grid{1},grid{4},grid{3});
+    depthSamples = reshape(depthSamples,[ToFFarr.N, ToFFarr.N]);
+
+    P = Pz*(K\[pixel';1]) + ToFFarr.d/2*[1;1;0];
+
+    % check how well surface fits
+    % TODO: check (s,t)
+    fitted_curve = @(s,t) sqrt(P(3).^2 + (s*ToFFarr.d/(ToFFarr.N-1)-P(1)).^2 + (t*ToFFarr.d/(ToFFarr.N-1)-P(2)).^2);
+
+    % get subset
+    [ii,jj] = meshgrid(1:ToFFarr.N);
+%     regionCut = true(ToFFarr.N, ToFFarr.N);
+    regionCut = abs(depthSamples - fitted_curve(ii,jj)) < 0.04;
+    notSaturated = depthSamples > 0.02;
+    subset = and(notSaturated, regionCut);
+    subset = subset(:);
+
+    % might be jj,ii ??
+    error = depthSamples(subset) - fitted_curve(ii(subset),jj(subset));
+    
+
+end
--- ToFFpipeline/refineDepthSearch.m
+++ ToFFpipeline/refineDepthSearch.m
@@ -8,30 +8,14 @@ function [depth, rmse, nPts, debug] = refineDepthSearch(dLF_fn, LFsize, K, initP
         ToFFarr
     end
     
-    % resample the LF to shift it correctly 
-    grid = getInterpGrid(K, ToFFarr, LFsize, initPz, pixel);
-    depthSamples = dLF_fn(grid{2},grid{1},grid{4},grid{3});
-    depthSamples = reshape(depthSamples,[ToFFarr.N, ToFFarr.N]);
+    [error, P, subset] = checkSurface(initPz, K, ToFFarr, LFsize, dLF_fn, pixel);
 
-    P = initPz*(K\[pixel';1]) + ToFFarr.d/2*[1;1;0];
-
-    % check how well surface fits
-    % TODO: check (s,t)
-    fitted_curve = @(s,t) sqrt(P(3).^2 + (s*ToFFarr.d/(ToFFarr.N-1)-P(1)).^2 + (t*ToFFarr.d/(ToFFarr.N-1)-P(2)).^2);
-
-    % get subset
-    [ii,jj] = meshgrid(1:ToFFarr.N);
-    regionCut = true(ToFFarr.N, ToFFarr.N);
-    notSaturated = depthSamples > 0.02;
-    subset = and(notSaturated, regionCut);
-    subset = subset(:);
-
-    error = depthSamples(subset) - fitted_curve(ii(subset),jj(subset));
+    
 
     % outputs
     depth = norm(P);
     rmse = rms(error);
-    nPts = length(subset);
+    nPts = sum(subset);
     debug.P = P;
 
     % threshold and refit
