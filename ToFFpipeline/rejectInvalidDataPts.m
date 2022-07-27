bee556264a641a97848a7d52ff0629c39e0cdb69
Adam T <ataras2@gmail.com>
2022-07-28 07:15:55 +1000

    adds saturation detection

--- ToFFpipeline/rejectInvalidDataPts.m
+++ ToFFpipeline/rejectInvalidDataPts.m
@@ -1,8 +1,9 @@
-function [indexSubset] = rejectInvalidDataPts (distGrid)
+function [indexSubset] = rejectInvalidDataPts (distGrid, threshold)
     % rejects saturated and similar points, returning a logical array of points to sample at
     arguments
-        distGrid (:,:) double
+        distGrid double
+        threshold double = 0.02
     end
 
-    
+    indexSubset = (distGrid > threshold);
 end
