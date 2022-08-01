93506182efade3801d35fd899fdbcb819f8f4c2e
Adam Taras <ataras2@gmail.com>
2022-08-02 09:57:38 +1000

    fixed theoretical surface to use Pz instead of radial dist

--- ToFFpipeline/getTheoreticalSurface.m
+++ ToFFpipeline/getTheoreticalSurface.m
@@ -1,16 +1,16 @@
-function [distGrid] = getTheoreticalSurface (pixel, radialDist, LFargs)
+function [distGrid] = getTheoreticalSurface (pixel, Pz, LFargs)
     % return the grid of distances to the pixel from all view, assuming no
     % occlusion
     arguments
         pixel (2,1) double % in form [k; l]
-        radialDist double
+        Pz double
         LFargs
     end
     
 %     P = radialDist2point(radialDist, LFargs, pixel);
+    P = Pz2point(Pz, LFargs, pixel);
 
-    sep = LFargs.baseline/(LFargs.N-1)
-    LFargs.seperation
+    sep = LFargs.baseline/(LFargs.N-1);
 
     fn = @(i,j) sqrt(P(3)^2 + (P(1) - (i-1)*sep).^2 + ...
                      (P(2) - (j-1)*sep).^2);
