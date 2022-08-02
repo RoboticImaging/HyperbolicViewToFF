05fe9b4363047db12e750cf3c4660a1202f19799
Adam Taras <ataras2@gmail.com>
2022-08-02 12:08:58 +1000

    added some occlusion detection methods

--- ToFFpipeline/rejectOcclusionOutliers.m
+++ ToFFpipeline/rejectOcclusionOutliers.m
@@ -1,8 +1,33 @@
-function [indexSubset] = rejectOcclusionOutliers (distGrid)
+function [indexSubset] = rejectOcclusionOutliers (distGrid, nvargs)
     % return a list of indices that are valid for curve fitting using segmentation on the grid data from other views
     arguments
         distGrid (:,:) double
+        nvargs.method string = 'none'
     end
 
-    indexSubset = true(size(distGrid));
+    middleIndex = (size(distGrid,1) + 1)/2;
+
+    if strcmp(nvargs.method, 'none')
+        indexSubset = true(size(distGrid));
+    elseif strcmp(nvargs.method, 'activecontour')
+        mask = zeros(size(distGrid));
+        mask(2:end-2, 2:end-2) = 1;
+        indexSubset = activecontour(distGrid, mask);
+
+        if indexSubset(middleIndex,middleIndex) == false
+            indexSubset  = ~indexSubset;
+        end
+    elseif strcmp(nvargs.method, 'lazysnapping')
+        % create superpixels
+        L = superpixels(distGrid, 10);
+
+        middle = sub2ind(size(distGrid), middleIndex, middleIndex);
+
+        % the foreground should contain the closest bit
+        [~, fgIdx] = min(distGrid(:));
+
+        indexSubset = lazysnapping(distGrid, L, middle, fgIdx);
+    else
+        error('Invalid occlusion mode selected %s', nvargs.method)
+    end
 end
